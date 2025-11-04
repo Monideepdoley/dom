02f6ef8c8de63223b9eb4203e557588d6636feb8
Monideepdoley <168443234+Monideepdoley@users.noreply.github.com>
2025-11-04 22:35:46 +0530

    Delete text.js

--- text.js
+++
@@ -1,8 +0,0 @@
-const mainImage = document.getElementById("displayed-image");
-const thumbnails = document.querySelectorAll(".thumbnails img");
-
-thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener("click", () => {
-    mainImage.src = thumbnail.src;
-  });
-});
